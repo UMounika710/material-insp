34658728297b30b494a69c5abe6537edc17402c3
UMounika710 <73570125+UMounika710@users.noreply.github.com>
2024-03-16 14:46:33 +0530

    Add files via upload

---
+++ employee-management/src/main/java/com/zettamine/employee/dao/EmployeeDAOImpl.java
@@ -0,0 +1,33 @@
+package com.zettamine.employee.dao;
+
+import java.util.List;
+
+import com.zettamine.employee.dto.Employee;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	@Override
+	public Integer saveEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findEmployeeById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEmployeeNameById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ employee-management/src/main/java/com/zettamine/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.zettamine.employee.service;
+
+import java.util.List;
+
+import com.zettamine.employee.dto.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Override
+	public Integer saveEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findEmployeeById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEmployeeNameById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
